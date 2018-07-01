b169a16f09d0cff096382f46651838adfc3da65a
biaedwards <bibby.georgieva@gmail.com>
2018-07-02 01:25:18 +0300

    header and gallery - COMPLETELY READY

--- js/general.js
+++ js/general.js
@@ -65,5 +65,37 @@ jQuery(document).ready(function ($) {
         }
         $shownMenu.removeClass('hide');
         $shownMenu.addClass('show');
-    })
+    });
+
+    (function(){
+        var scrollHandle = 0;
+        var scrollStep = 5;
+        var $parent = $('#inspo-images');
+
+        $('.panner').on('mouseenter', function(){
+            var data = $(this).data('scrollModifier'), direction = parseInt(data, 10);
+            $(this).addClass('active');
+
+            startScrolling(direction, scrollStep);
+        });
+
+        $('.panner').on('mouseleave', function(){
+            stopScrolling();
+            $(this).removeClass('active');
+        });
+
+        function startScrolling(modifier, step){
+            if(scrollHandle === 0){
+                scrollHandle = setInterval(function(){
+                    var newOffset = $parent.scrollLeft() + (scrollStep*modifier);
+                    $parent.scrollLeft(newOffset);
+                },10);
+            }
+        }
+
+        function stopScrolling(){
+            clearInterval(scrollHandle);
+            scrollHandle = 0;
+        }
+    }());
 });
